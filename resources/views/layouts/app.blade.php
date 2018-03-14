c138053f2a9ba8ce4c74b03c9f9ce23cfb13e9fe
Roger Forner Fabre <sir.rogerforner@gmail.com>
2018-03-14 21:16:54 +0100

    Favicon Ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,17 @@
 
   <title>{{ config('app.name', 'eShared') }}</title>
 
+  <!-- Favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icons/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('icons/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('icons/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('icons/site.webmanifest') }}">
+  <link rel="mask-icon" href="{{ asset('icons/safari-pinned-tab.svg') }}" color="#f2f698">
+  <link rel="shortcut icon" href="{{ asset('icons/favicon.ico') }}">
+  <meta name="msapplication-TileColor" content="#f2f698">
+  <meta name="msapplication-config" content="{{ asset('icons/browserconfig.xml') }}">
+  <meta name="theme-color" content="#f2f698">
+
   <!-- Styles -->
   <script src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
